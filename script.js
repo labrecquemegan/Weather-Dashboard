3a6aad81f039222368129f2c3b1af513cc7228bd
ML <labrecquemegan@hotmail.com>
2022-01-29 10:52:28 -0500

    minor changes, wrote out API key/URL

---
+++ script.js
@@ -0,0 +1,14 @@
+// function to save to local storage
+// a way to pull items from local storage
+// utalize weather api
+// use info from yhe API to build the display
+// dynamically change based on city
+
+// global variables
+var searchHistory = []
+var weatherApiUrl = "https://api.openweathermap.org"
+var weatherApiKey = "221d8241c6e1a65fa67ef2229b2a5fc1"
+
+// DOM elements
+
+// functions
