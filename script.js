7ab2be66fc60c61f2d9cc96bb6ca90e8b5338b2e
ML <labrecquemegan@hotmail.com>
2022-01-31 09:37:29 -0500

    worked with tutor to put in my fetch request

--- script.js
+++ script.js
@@ -8,6 +8,27 @@
 var searchHistory = []
 var weatherApiUrl = "https://api.openweathermap.org"
 var weatherApiKey = "221d8241c6e1a65fa67ef2229b2a5fc1"
+var cityName = "Chicago"
+var query = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + weatherApiKey
+
+fetch(query) 
+// if you get a response from the API, then it converts it to JSON so JS can read
+.then(res => res.json())
+// THEN, get data
+.then(data =>{
+    var lat = data.coord.lat
+    var lon = data.coord.lon
+    var querry2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + weatherApiKey
+
+    fetch(querry2)
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data)
+    })
+})
+
+
+
 
 // DOM elements
 
